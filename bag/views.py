7f140eb7f70ccea50fb705184881cabc41468ca5
JakBul <>
2023-11-11 11:47:03 +0000

    Add adjust bag and remove from bag views

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 
 def view_bag(request):
@@ -25,3 +25,37 @@ def add_to_bag(request, item_id):
     # overwrite the variable with the updated version
     request.session['bag'] = bag
     return redirect(redirect_url)
+
+
+def adjust_bag(request, item_id):
+    """ Adjust the quantity of the specified product to the specified amount """
+
+    quantity = int(request.POST.get('quantity'))
+
+    # while in the session, find the bag or create one
+    bag = request.session.get('bag', {})
+
+    # adjust the quantity
+    if quantity > 0:
+        bag[item_id] = quantity
+    else:
+        bag.pop(item_id)
+
+    # overwrite the variable with the updated version
+    request.session['bag'] = bag
+    return redirect(reverse('view_bag'))
+
+def remove_from_bag(request, item_id):
+    """ Remove the item from the shopping bag """
+    
+    try:
+        # while in the session, find the bag or create one
+        bag = request.session.get('bag', {})
+
+        bag.pop(item_id)
+
+        # overwrite the variable with the updated version
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
+    except Exception as e:
+        return HttpResponse(status=500)
