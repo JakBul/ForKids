a471a46f7872938a9f752f6f5322bfbb03807704
JakBul <>
2023-11-21 20:16:34 +0000

    Set debug

--- ForKids/settings.py
+++ ForKids/settings.py
@@ -25,14 +25,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY", '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = [
     '8000-jakbul-forkids-h36jw8bss7.us2.codeanyapp.com',
-    'https://for-kids-1eb46f197086.herokuapp.com /',
+    'https://for-kids-1eb46f197086.herokuapp.com/',
 ]
 
 
